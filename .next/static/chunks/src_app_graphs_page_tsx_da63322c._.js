96826d0be7f94bf1cebc08ecd587727dbefb1f53
DotmacTech <dotmacautomations@gmail.com>
2025-04-10 17:05:15 +0100

    Add .next/static/chunks/src_app_graphs_page_tsx_da63322c._.js 1744301111957

---
+++ .next/static/chunks/src_app_graphs_page_tsx_da63322c._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_screens_Graphs_GraphsScreen_tsx_42403e6b._.js"
+],
+    source: "dynamic"
+});
