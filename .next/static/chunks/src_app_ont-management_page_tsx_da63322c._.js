aee8daeef8205f43b14f0736746d4eba5d0faf24
DotmacTech <dotmacautomations@gmail.com>
2025-04-10 17:05:31 +0100

    Add .next/static/chunks/src_app_ont-management_page_tsx_da63322c._.js 1744301127960

---
+++ .next/static/chunks/src_app_ont-management_page_tsx_da63322c._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_screens_ONTManagement_ONTManagementScreen_tsx_2d6e10fd._.js"
+],
+    source: "dynamic"
+});
